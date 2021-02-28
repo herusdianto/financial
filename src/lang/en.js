7ab851357de8646103a1469df502a803644f18df
Heru <heru.rusdianto.93@gmail.com>
2021-03-01 00:01:51 +0700

    create en & id lang files

---
+++ src/lang/en.js
@@ -0,0 +1,20 @@
+import { en } from 'vuetify/lib/locale'
+
+export default {
+    ...en,
+
+    title: 'Financial Transactions',
+
+    menu: {
+        title: 'Menu',
+        list: 'Menu List',
+    },
+
+    income: {
+        title: 'Income',
+    },
+
+    outcome: {
+        title: 'Outcome',
+    },
+}
---
+++ src/lang/id.js
@@ -0,0 +1,20 @@
+import { id } from 'vuetify/lib/locale'
+
+export default {
+    ...id,
+
+    title: 'Transaksi Keuangan',
+
+    menu: {
+        title: 'Menu',
+        list: 'Daftar Menu',
+    },
+
+    income: {
+        title: 'Pemasukan',
+    },
+
+    outcome: {
+        title: 'Pengeluaran',
+    },
+}
