2b0d8b3ca075fabacb282787ac08fc63b2d45c3b
Heru <heru.rusdianto.93@gmail.com>
2021-03-01 00:02:27 +0700

    add locale config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+import en from '../lang/en'
+import id from '../lang/id'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    lang: {
+        locales: { en, id },
+        current: 'en',
+    },
 });
